261f62899d9e5c469d55f1df82b66a2826bc2e9c
Rebecca <rriley513@gmail.com>
2022-12-14 14:01:59 -0500

    Use sample size from h5 if available, instead of defaults

--- util.py
+++ util.py
@@ -277,6 +277,7 @@ def process_opts(opts, summary_stats = False):
     param_names = [p.name for p in parameters]
 
     real = False
+    ss_total = global_vars.DEFAULT_SAMPLE_SIZE
     # if real data provided
     if opts.data_h5 is not None: # h5 is None option at end of func
         real = True
@@ -290,6 +291,7 @@ def process_opts(opts, summary_stats = False):
         # most typical case for real data
         iterator = real_data_random.RealDataRandomIterator(opts.data_h5,
             opts.bed)
+        ss_total = iterator.num_samples
 
     # parse model and simulator
     if opts.model == 'const':
@@ -335,8 +337,7 @@ def process_opts(opts, summary_stats = False):
         print("FILTERING SINGLETONS")
 
     # generator
-    sample_size_total = global_vars.DEFAULT_SAMPLE_SIZE if opts.sample_size is \
-        None else opts.sample_size
+    sample_size_total = ss_total if opts.sample_size is None else opts.sample_size
     sample_sizes = [sample_size_total//num_pops for i in range(num_pops)]
 
     gen = generator.Generator(simulator, param_names, sample_sizes,
