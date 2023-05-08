739eccfb33d5a8bc85e81bc40e3a1c6ef50c41e4
Rebecca <rriley513@gmail.com>
2023-05-08 17:11:56 -0400

    Simplify param set and parameter usage

--- generator.py
+++ generator.py
@@ -10,7 +10,7 @@ from numpy.random import default_rng
 
 # our imports
 import global_vars
-import param_set
+from param_set import ParamSet
 import simulation
 import util
 
@@ -20,14 +20,13 @@ import util
 
 class Generator:
 
-    def __init__(self, simulator, param_names, sample_sizes, seed,
+    def __init__(self, simulator, iterable_params, sample_sizes, seed,
         mirror_real=False, reco_folder=""):
         self.simulator = simulator
-        self.param_names = param_names
+        self.iterable_params = iterable_params
         self.sample_sizes = sample_sizes
         self.num_samples = sum(sample_sizes)
         self.rng = default_rng(seed)
-        self.curr_params = None
 
         # for real data, use HapMap
         if mirror_real and reco_folder != None:
@@ -38,7 +37,7 @@ class Generator:
         else:
             self.prior, self.weights = [], []
 
-    def simulate_batch(self, batch_size=global_vars.BATCH_SIZE, params=[],
+    def simulate_batch(self, batch_size=global_vars.BATCH_SIZE, params=[], 
         region_len=False, real=False, neg1=True):
 
         # initialize 4D matrix (two channels for distances)
@@ -49,20 +48,20 @@ class Generator:
                 global_vars.NUM_SNPS, 2), dtype=np.float32) # two channels
 
         # set up parameters
-        sim_params = param_set.ParamSet(self.simulator)
+        sim_params = ParamSet(self.simulator)
         if real:
             pass # keep orig for "fake" real
         elif params == []:
-            sim_params.update(self.param_names, self.curr_params)
+            sim_params.update(self.iterable_params)
         else:
-            sim_params.update(self.param_names, params)
+            sim_params.update(params)
 
         # simulate each region
         for i in range(batch_size):
             seed = self.rng.integers(1,high=2**32) # like GAN "noise"
 
             ts = self.simulator(sim_params, self.sample_sizes, seed,
-                self.get_reco(sim_params))
+                self.get_reco(sim_params.get("reco")))
             region = prep_region(ts, neg1, region_len=region_len)
 
             if region_len:
@@ -78,11 +77,11 @@ class Generator:
             region_len=region_len)
 
     def update_params(self, new_params):
-        self.curr_params = new_params
+        self.iterable_params = new_params
 
-    def get_reco(self, params):
+    def get_reco(self, reco):
         if self.prior == []:
-            return params.reco.value
+            return reco
 
         return draw_background_rate_from_prior(self.prior, self.weights,
             self.rng)
@@ -108,12 +107,13 @@ def prep_region(ts, neg1, region_len):
 if __name__ == "__main__":
 
     batch_size = 50
-    params = param_set.ParamSet()
+    simulator = simulation.simulate_exp
+    params = get_param_set(simulator)
 
     # quick test
     print("sim exp")
-    generator = Generator(simulation.simulate_exp, ["N1", "T1"], [20],
+    generator = Generator(simulator, ["N1", "T1"], [20],
                           global_vars.DEFAULT_SEED)
-    generator.update_params([params.N1.value, params.T1.value])
+    generator.update_params([params["N1"].value, params["T1"].value])
     mini_batch = generator.simulate_batch(batch_size=batch_size)
     print("x", mini_batch.shape)
